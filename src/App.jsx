46dc200a4d923a73797fb3060d9abf94f7ddd0cc
Suraj Randhari <surajrandhari@gmail.com>
2025-03-08 12:55:41 +0530

    implement the react router dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,35 @@
-import React from 'react'
 
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import Dashboard from './pages/Dashboard/Dashboard';
+import About from './pages/About/About';
+import Layout from './Layout/Layout';
 const App = () => {
-  return (
-    <div>App</div>
-  )
+
+
+  const router = createBrowserRouter([
+
+    {
+      path: '/',
+      element: <Layout />,
+      children: [
+        {
+          path: '/',
+          element: <Dashboard />
+        },
+        {
+          path:'/about',
+          element:<About/>
+        }
+      ]
+    },
+   
+
+  ])
+
+
+  return <RouterProvider router={router} />
+
 }
 
 export default App
