e0fd7513b6867530a2d6204621d809dfd7f4e390
Suraj Randhari <surajrandhari@gmail.com>
2025-03-08 12:54:06 +0530

    layout configure

--- src/Layout/Layout.jsx
+++ src/Layout/Layout.jsx
@@ -1,9 +1,23 @@
-import React from 'react'
+import { Outlet } from "react-router-dom";
+import Navbar from "../components/Navbar";
+import Sidebar from "../components/Sidebar";
 
 const Layout = () => {
   return (
-    <div>Layout</div>
-  )
+    <div className="layout">
+      <div className="navbar">
+        <Navbar />
+      </div>
+      <div className="main-flex-layout">
+        <div className="sidebar">
+          <Sidebar />
+        </div>
+        <div className="outlet">
+          <Outlet />
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default Layout
+export default Layout;
